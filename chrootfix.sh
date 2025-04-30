9d31329c939620d74628df369562b1da2119de83
claudemods <73653396+claudemods@users.noreply.github.com>
2025-04-30 12:22:29 +0100

    Delete chrootfix.sh

--- chrootfix.sh
+++
@@ -1,3 +0,0 @@
-sudo arch-chroot /mnt /bin/bash -c grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
-sudo arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-sudo arch-chroot /mnt mkinitcpio -P
